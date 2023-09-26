ea994f22e43e5e8e2e37778714b44c06fb38ade1
Jonathan Faus <jonfaugom@alu.edu.gva.es>
2023-09-26 16:33:04 +0200

    17 hecha

---
+++ app/src/main/java/com/example/tema2actividades/Actividad17/Libro.kt
@@ -0,0 +1,16 @@
+package com.example.tema2actividades.Actividad17
+
+open class Libro (private val titulo: String,private val autor :String, private val any:Int,private val precio: Double) {
+    override fun toString(): String {
+        return "Titulo: $titulo\n Autor: $autor\n Año: $any\n Precio: $precio\n"
+    }
+    open fun calcularPrecioDescuento():Double{
+        return precio-precio*0.10
+    }
+    fun getTitulo():String{
+        return titulo
+    }
+    fun getPrecio():Double{
+        return precio
+    }
+}
